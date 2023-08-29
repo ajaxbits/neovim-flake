ef8a6ff1c466af4cc433744f91447705c3ef5656
Alex Jackson <contact@ajaxbits.com>
2023-08-29 11:12:44 -0500

    fix(gitsigns): make keymaps work

--- modules/git/git.nix
+++ modules/git/git.nix
@@ -22,48 +22,36 @@ in {
 
   config =
     mkIf cfg.enable
-    (
-      let
-        mkVimBool = val:
-          if val
-          then "1"
-          else "0";
-      in {
-        vim.startPlugins = with pkgs.neovimPlugins;
-          if (cfg.gitsigns.enable)
-          then [gitsigns-nvim]
-          else [];
+    {
+      vim.startPlugins = with pkgs.neovimPlugins;
+        if (cfg.gitsigns.enable)
+        then [gitsigns-nvim]
+        else [];
 
-        vim.luaConfigRC = mkIf (cfg.gitsigns.enable) ''
-          -- GitSigns setup
-          require('gitsigns').setup {
-            keymaps = {
-              noremap = true,
+      vim.luaConfigRC = mkIf (cfg.gitsigns.enable) ''
+        -- GitSigns setup
+        require('gitsigns').setup {
+          on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
 
-              ['n <leader>gn'] = { expr = true, "&diff ? \'\' : '<cmd>Gitsigns next_hunk<CR>'"},
-              ['n <leader>gp'] = { expr = true, "&diff ? \'\' : '<cmd>Gitsigns prev_hunk<CR>'"},
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
 
-              ['n <leader>gs'] = '<cmd>Gitsigns stage_hunk<CR>',
-              ['v <leader>gs'] = ':Gitsigns stage_hunk<CR>',
-              ['n <leader>gu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-              ['n <leader>gr'] = '<cmd>Gitsigns reset_hunk<CR>',
-              ['v <leader>gr'] = ':Gitsigns reset_hunk<CR>',
-              ['n <leader>gR'] = '<cmd>Gitsigns reset_buffer<CR>',
-              ['n <leader>gp'] = '<cmd>Gitsigns preview_hunk<CR>',
-              ['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-              ['n <leader>gS'] = '<cmd>Gitsigns stage_buffer<CR>',
-              ['n <leader>gU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
-              ['n <leader>gts'] = ':Gitsigns toggle_signs<CR>',
-              ['n <leader>gtn'] = ':Gitsigns toggle_numhl<CR>',
-              ['n <leader>gtl'] = ':Gitsigns toggle_linehl<CR>',
-              ['n <leader>gtw'] = ':Gitsigns toggle_word_diff<CR>',
-
-              -- Text objects
-              ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
-              ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
-            },
-          }
-        '';
-      }
-    );
+            map('n', '<leader>gs', gs.stage_hunk)
+            map('v', '<leader>gs', gs.stage_hunk)
+            map('n', '<leader>gr', gs.reset_hunk)
+            map('v', '<leader>gr', gs.reset_hunk)
+            map('n', '<leader>gr', gs.reset_buffer)
+            map('n', '<leader>gb', gs.blame_line{full=true})
+            map('n', '<leader>gts', gs.toggle_signs)
+            map('n', '<leader>gtn', gs.toggle_numhl)
+            map('n', '<leader>gtl', gs.toggle_linehl)
+            map('n', '<leader>gtw', gs.toggle_word_diff)
+          },
+        }
+      '';
+    };
 }
