009a004ede4fb39315b1519703c9b0e4fe3a710e
Alex Jackson <contact@ajaxbits.com>
2023-08-29 12:09:22 -0500

    fix: fix config error with gitsigns

--- modules/git/git.nix
+++ modules/git/git.nix
@@ -30,7 +30,7 @@ in {
 
       vim.luaConfigRC = mkIf (cfg.gitsigns.enable) ''
         -- GitSigns setup
-        require('gitsigns').setup {
+        require('gitsigns').setup{
           on_attach = function(bufnr)
             local gs = package.loaded.gitsigns
 
@@ -40,17 +40,36 @@ in {
               vim.keymap.set(mode, l, r, opts)
             end
 
-            map('n', '<leader>gs', gs.stage_hunk)
-            map('v', '<leader>gs', gs.stage_hunk)
+            -- Navigation
+            map('n', ']c', function()
+              if vim.wo.diff then return ']c' end
+              vim.schedule(function() gs.next_hunk() end)
+              return '<Ignore>'
+            end, {expr=true})
+
+            map('n', '[c', function()
+              if vim.wo.diff then return '[c' end
+              vim.schedule(function() gs.prev_hunk() end)
+              return '<Ignore>'
+            end, {expr=true})
+
+            -- Actions
             map('n', '<leader>gr', gs.reset_hunk)
-            map('v', '<leader>gr', gs.reset_hunk)
-            map('n', '<leader>gr', gs.reset_buffer)
-            map('n', '<leader>gb', gs.blame_line{full=true})
-            map('n', '<leader>gts', gs.toggle_signs)
-            map('n', '<leader>gtn', gs.toggle_numhl)
-            map('n', '<leader>gtl', gs.toggle_linehl)
-            map('n', '<leader>gtw', gs.toggle_word_diff)
-          },
+            map('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+            map('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+            map('n', '<leader>gS', gs.stage_buffer)
+            map('n', '<leader>gu', gs.undo_stage_hunk)
+            map('n', '<leader>gR', gs.reset_buffer)
+            map('n', '<leader>gp', gs.preview_hunk)
+            map('n', '<leader>gb', function() gs.blame_line{full=true} end)
+            map('n', '<leader>gb', gs.toggle_current_line_blame)
+            map('n', '<leader>gd', gs.diffthis)
+            map('n', '<leader>gD', function() gs.diffthis('~') end)
+            map('n', '<leader>gd', gs.toggle_deleted)
+
+            -- Text object
+            map({'o', 'x'}, 'ig', ':<C-U>Gitsigns select_hunk<CR>')
+          end
         }
       '';
     };
