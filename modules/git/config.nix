0418575832f3b111c121572e5d786fc770f0253d
Alex Jackson <contact@ajaxbits.com>
2023-08-31 09:57:48 -0500

    feat: add git opening shortcuts

--- modules/git/config.nix
+++ modules/git/config.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  config,
   lib,
   ...
 }:
@@ -9,6 +8,10 @@ with lib; {
     vim.git = {
       enable = mkDefault false;
       gitsigns.enable = mkDefault false;
+    };
+    vim.nnoremap = {
+      "<silent><leader>gor" = ":! ${pkgs.gh}/bin/gh repo view -w <CR>";
+      "<silent><leader>gop" = ":! ${pkgs.gh}/bin/gh pr view -w <CR>";
     };
   };
 }
